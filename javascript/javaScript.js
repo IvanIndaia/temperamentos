06a0080f564a93622e328f9f3b3f695f4702650a
Ivan Oliveira <79813537+IvanIndaia@users.noreply.github.com>
2022-11-03 11:04:08 -0300

    PrimeiroTeste-Luana

--- javascript/javaScript.js
+++ javascript/javaScript.js
@@ -10,7 +10,7 @@ function starter() {
     var checkRadio = document.querySelectorAll('input')
     let cr = 1
 
-    let S = 0
+    S = 0
     N = 0
     SN = 0
     COL = 0
@@ -22,26 +22,13 @@ function starter() {
     nMEL = 0
     nFLE = 0
 
+
     alerta = true
     med = 48
 
     temperamento = []
     naoTemp = []
 
-    // for (let lin = 1; lin <= 48; lin++) {
-    //     for (let col = 1; col <= 3; col++) {
-    //         // console.log('CHECADO!!!')
-    //         matriz[[lin][col]] = checkRadio[cr].checked
-    //         console.log('Matriz [' + lin + ']' + '[' + col + '] POS: ' + lin + '  = ' + matriz[[lin][col]])
-    //         cr++
-    //     }
-
-    // }
-
-    /*
-        Lin = Perguntas
-        CR = Vetor
-    */
     for (let lin = 1; lin <= 48; lin++) {
         for (let col = 1; col <= 3; col++) {
             if (checkRadio[cr].checked) {
@@ -149,16 +136,31 @@ function starter() {
         }
     }
 
+    validar49e50()
+    // alert(`Reprovação é igual a: ${reprovado}`)
+    const49e50()
+    resultadoFinal()
     let validador = S + N + SN
     // console.log(`S = ${S}   N = ${N}   SN = ${SN}`)
-    console.log(`validador vale: ${validador}`)
-    if (validador != 4) {
+
+    // console.log(`validador vale: ${validador}`)
+    if (validador < 48) {
         alert('Responda todas as Perguntas!!!')
         console.clear()
+    } else if (reprovado == true){
+        alert('Nas questões 49 e 50 tem que escolher: \nNo mínimo 1 alternativa e no máximo 2!')
+
+        var grups = document.getElementsByClassName('tab-pane')
+        for (i = 0; i < 7; i++) {
+            grups[i].classList.remove('active')
+        }
+        grups[5].classList.add('active')
+        console.clear()
+
     } else {
         var grups = document.getElementsByClassName('tab-pane')
 
-        for (i = 0; i < 6; i++) {
+        for (i = 0; i < 7; i++) {
             grups[i].classList.remove('active')
         }
         grups[6].classList.add('active')
@@ -168,7 +170,7 @@ function starter() {
         document.getElementById('next').disabled = true
 
         med -= SN
-        console.log('O Total de SIM foi: ' + S + '\n O Total de NÃO foi: ' + N)
+        console.log('O Total de SIM foi: ' + S + '\nO Total de NÃO foi: ' + N)
         console.log('Lista SIM => ' + temperamento + '\nLista NÃO => ' + naoTemp)
         console.log('Colerico = ' + COL + '\nSanguíneo = ' + SAN + '\nMelancólico = ' + MEL + '\nFleumático = ' + FLE)
         console.log('Não Colerico = ' + nCOL + '\nNão Sanguíneo = ' + nSAN + '\nNão Melancólico = ' + nMEL + '\nNão Fleumático = ' + nFLE)
@@ -178,10 +180,9 @@ function starter() {
     /* ===== FIM ===== */
 }
 
-
 aba = 0
 function avancar() {
-    console.log("Click no AVANÇAR")
+    // console.log("Click no AVANÇAR")
 
     var grupos = document.getElementsByClassName('tab-pane')
 
@@ -216,7 +217,7 @@ function avancar() {
 }
 
 function voltar() {
-    console.log("Click no VOLTAR")
+    // console.log("Click no VOLTAR")
     var grupos = document.getElementsByClassName('tab-pane')
 
     if (aba == 5) {
@@ -249,4 +250,221 @@ function voltar() {
 
 function repetir() {
     $(location).attr('href', 'index.html')
+}
+
+function validar49e50() {
+    reprovado = false
+    let ckb49 = document.getElementsByClassName('49')
+    let ckb50 = document.getElementsByClassName('50')
+    var m49 = 0
+    var m50 = 0
+
+    for (i =0; i < ckb49.length; i++){
+        if (ckb49[i].checked) {
+            console.log("Pergunta 49: "+ckb49[i].value +" Marcado")
+            m49++
+        }
+    }
+
+    for (i =0; i < ckb50.length; i++){
+        if (ckb50[i].checked) {
+            console.log("Pergunta 50: "+ckb50[i].value +" Marcado")
+            m50++
+        }
+    }
+    // console.log(m49)
+    // console.log(m50)
+
+    if (m49 > 2 || m49 < 1 || m50 > 2 || m50 < 1){
+        reprovado = true
+    }
+    // console.log(ckb.length)
+}
+
+function const49e50(){
+
+    let ckbox49 = document.getElementsByClassName('49')
+    let ckbox50 = document.getElementsByClassName('50')
+    s49 = 0
+    s50 = 0 
+
+    /* ===================================== */
+    /* ================ SIM ================ */
+    /* ===================================== */
+    for (i =0; i < ckbox49.length; i++){
+        if (ckbox49[i].checked) {
+            if (ckbox50[i].value == 'A'){
+                s49++
+                COL++
+                // alert('49 Colérico')
+            }
+            if (ckbox50[i].value == 'B'){
+                s49++
+                FLE++
+                // alert('49 Fleumático')
+            }
+            if (ckbox50[i].value == 'C'){
+                s49++
+                MEL++
+                // alert('49 Melancólico')
+            }
+            if (ckbox50[i].value == 'D'){
+                s49++
+                SAN++
+                // alert('49 Sanguíneo')
+            }
+        }
+    }
+
+    for (i =0; i < ckbox50.length; i++){
+        if (ckbox50[i].checked) {
+            if (ckbox49[i].value == 'A'){
+                s50++
+                FLE++
+                // alert('50 Fleumático')
+            }
+            if (ckbox49[i].value == 'B'){
+                s50++
+                MEL++
+                // alert('50 Melancólico')
+            }
+            if (ckbox49[i].value == 'C'){
+                s50++
+                COL++
+                // alert('50 Colérico')
+            }
+            if (ckbox49[i].value == 'D'){
+                s50++
+                SAN++
+                // alert('50 Sanguíneo')
+            }
+        }
+    }
+
+    /* ===================================== */
+    /* ================ NÃO ================ */
+    /* ===================================== */
+
+    for (i =0; i < ckbox49.length; i++){
+        if (!ckbox49[i].checked) {
+            if (ckbox50[i].value == 'A'){
+                nCOL++
+                // alert('49 Colérico')
+            }
+            if (ckbox50[i].value == 'B'){
+                nFLE++
+                // alert('49 Fleumático')
+            }
+            if (ckbox50[i].value == 'C'){
+                nMEL++
+                // alert('49 Melancólico')
+            }
+            if (ckbox50[i].value == 'D'){
+                nSAN++
+                // alert('49 Sanguíneo')
+            }
+        }
+    }
+
+    for (i =0; i < ckbox50.length; i++){
+        if (!ckbox50[i].checked) {
+            if (ckbox49[i].value == 'A'){
+                nFLE++
+                // alert('50 Fleumático')
+            }
+            if (ckbox49[i].value == 'B'){
+                nMEL++
+                // alert('50 Melancólico')
+            }
+            if (ckbox49[i].value == 'C'){
+                nCOL++
+                // alert('50 Colérico')
+            }
+            if (ckbox49[i].value == 'D'){
+                nSAN++
+                // alert('50 Sanguíneo')
+            }
+        }
+    }
+}
+
+function resultadoFinal(){
+
+    var resultadoCOL = COL / S * 100 
+    var resultadoSAN = SAN / S * 100
+    var resultadoMEL = MEL / S * 100
+    var resultadoFLE = FLE / S * 100
+
+    var resultadonCOL = nCOL / N * 100
+    var resultadonSAN = nSAN / N * 100
+    var resultadonMEL = nMEL / N * 100
+    var resultadonFLE = nFLE / N * 100
+    
+    console.log(`COL ${resultadoCOL.toFixed(2)}%
+            \nSAN ${resultadoSAN.toFixed(2)}%
+            \nMEL ${resultadoMEL.toFixed(2)}%
+            \nFLE ${resultadoFLE.toFixed(2)}%
+            \n\nnCOL ${resultadonCOL.toFixed(2)}%
+            \nnSAN ${resultadonSAN.toFixed(2)}%
+            \nnMEL ${resultadonMEL.toFixed(2)}%
+            \nnFLE ${resultadonFLE.toFixed(2)}%
+    `)
+
+    // let progress = document.getElementsByClassName('myBar')
+    let progress = $('.myBar')
+
+
+
+    let simCOL = $('#smyBarCOL')
+    // console.log(simCOL)
+    simCOL.css({
+        "width":`${resultadoCOL.toFixed(2)}%`
+    })
+    
+    let simSAN = $('#smyBarSAN')
+    // console.log(simSAN)
+    simSAN.css({
+        "width":`${resultadoSAN.toFixed(2)}%`
+    })
+    
+    let simMEL = $('#smyBarMEL')
+    // console.log(simMEL)
+    simMEL.css({
+        "width":`${resultadoMEL.toFixed(2)}%`
+    })
+    
+    let simFLE = $('#smyBarFLE')
+    // console.log(simFLE)
+    simFLE.css({
+        "width":`${resultadoFLE.toFixed(2)}%`
+    })
+
+
+
+    let naoCOL = $('#nmyBarCOL')
+    // console.log(naoCOL)
+    naoCOL.css({
+        "width":`${resultadonCOL.toFixed(2)}%`
+    })
+    
+    let naoSAN = $('#nmyBarSAN')
+    // console.log(naoSAN)
+    naoSAN.css({
+        "width":`${resultadonSAN.toFixed(2)}%`
+    })
+    
+    let naoMEL = $('#nmyBarMEL')
+    // console.log(naoMEL)
+    naoMEL.css({
+        "width":`${resultadonMEL.toFixed(2)}%`
+    })
+    
+    let naoFLE = $('#nmyBarFLE')
+    // console.log(naoFLE)
+    naoFLE.css({
+        "width":`${resultadonFLE.toFixed(2)}%`
+    })
+
+
+
 }
